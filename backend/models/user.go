f432060b43708cd9c59f665f66fa3a7cc1ce399c
Yusuke Shimizu <44394399+ushmz@users.noreply.github.com>
2021-07-26 23:48:49 +0900

    :+1: Enable re-register

--- backend/models/user.go
+++ backend/models/user.go
@@ -24,8 +24,22 @@ type User struct {
 	CreatedAt string `db:"created_at" json:"createdAt"`
 }
 
+type ExistUser struct {
+	// Id : The ID of user.
+	Id int64 `db:"id" json:"id"`
+
+	// Uid : User name/Id for label.
+	Uid string `db:"uid" json:"uid"`
+
+	// Secret : Generated secret string.
+	Secret string `db:"generated_secret" json:"secret"`
+}
+
 // UserResponse : Struct for response body of `CreateUser` handler
 type UserResponse struct {
+	// Exist : Given uid is exist on DB ot not.
+	Exist bool `json:"exist"`
+
 	// UserId : Unique Id used in koolhaas DB.
 	UserId int64 `json:"user"`
 
