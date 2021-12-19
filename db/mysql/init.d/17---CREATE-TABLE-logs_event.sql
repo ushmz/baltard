0221ef7dd8ea899d71793b1101aef6579da8dcae
Yusuke Shimizu <44394399+ushmz@users.noreply.github.com>
2021-12-19 13:11:08 +0900

    :card_file_box: Fix DB structure

--- db/mysql/init.d/17---CREATE-TABLE-logs_event.sql
+++ db/mysql/init.d/17---CREATE-TABLE-logs_event.sql
@@ -1,5 +1,5 @@
 -- +migrate Up
-CREATE TABLE `behavior_logs_click` (
+CREATE TABLE `logs_event` (
   `id` int NOT NULL AUTO_INCREMENT,
   `user_id` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `task_id` int NOT NULL,
@@ -8,6 +8,7 @@ CREATE TABLE `behavior_logs_click` (
   `serp_page` int NOT NULL DEFAULT '0',
   `serp_rank` int NOT NULL DEFAULT '0',
   `is_visible` tinyint(1) NOT NULL DEFAULT '0',
+  `event` varchar(10) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
