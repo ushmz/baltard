a720f58f6c6d70c4999a921356369eb82cf9eb17
Yusuke Shimizu <44394399+ushmz@users.noreply.github.com>
2021-12-25 22:52:54 +0900

    :white_check_mark: Add log handler test

--- app/internal/handler/log_test.go
+++ app/internal/handler/log_test.go
@@ -1,26 +1,60 @@
 package handler
 
 import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 
+	"ratri/internal/domain/model"
+	mock "ratri/internal/mock/usecase"
+
 	"github.com/golang/mock/gomock"
+	"github.com/google/go-cmp/cmp"
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
 )
 
 var (
-	timeLog = `{
-		"user": 999,
-		"time": 999,
-		"task": 5,
-		"condision": 5
+	serpViewLog = model.SerpViewingLogParam{
+		UserId:      999,
+		TaskId:      5,
+		ConditionId: 3,
+	}
+	serpViewLogBlob = `{
+		"user":      999,
+		"task":      5,
+		"condition": 3
 	}`
-	clickLog = `{
-		"user": 999,
-		"task_id": 5,
-		"conditionId": 5,
-		"time": 999,
-		"page": 999,
-		"rank": 999,
-		"visible": false
+	serpEventLog = model.SearchPageEventLogParam{
+		User:        42,
+		TaskId:      5,
+		ConditionId: 3,
+		Time:        42,
+		Page:        1,
+		Rank:        3,
+		IsVisible:   true,
+		Event:       "click",
+	}
+	serpEventLogBlob = `{
+		"user":      42,
+		"task":      5,
+		"condition": 3,
+		"time":      42,
+		"page":      1,
+		"rank":      3,
+		"visible":   true,
+		"event":     "click"
+	}`
+	searchSessionLog = model.SearchSession{
+		UserId:      42,
+		TaskId:      5,
+		ConditionId: 3,
+	}
+	searchSessionLogBlob = `{
+		"user":      42,
+		"task":      5,
+		"condition": 3
 	}`
 )
 
@@ -28,34 +62,76 @@ func TestCreateTaskTimeLog(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
-	// logDaoMock := dao.NewMockLog(ctrl)
-	// e := echo.New()
-	// req := httptest.NewRequest(http.MethodPost, "/v1/users/log/time", strings.NewReader(timeLog))
+	mock := mock.NewMockLog(ctrl)
+	mock.EXPECT().CumulateSerpViewingTime(&serpViewLog).Return(nil)
 
-	// req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
-	// rec := httptest.NewRecorder()
-	// c := e.NewContext(req, rec)
-	// h := &Handler{DB: mockDB}
+	e := echo.New()
+	req := httptest.NewRequest(
+		http.MethodPost,
+		"/v1/logs/time",
+		strings.NewReader(serpViewLogBlob),
+	)
 
-	// if assert.NoError(t, h.CreateTaskTimeLog(c)) {
-	// 	if diff := cmp.Diff(rec.Code, http.StatusCreated); diff != "" {
-	// 		t.Errorf("Status code does not match.\n%v", diff)
-	// 	}
-	// }
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	h := NewLogHandler(mock)
+
+	if assert.NoError(t, h.CumulateSerpViewingTime(c)) {
+		if diff := cmp.Diff(rec.Code, http.StatusCreated); diff != "" {
+			t.Fatal("Status code does not match.\n" + diff)
+		}
+	}
 }
 
 func TestCreateSerpClickLog(t *testing.T) {
-	// e := echo.New()
-	// req := httptest.NewRequest(http.MethodPost, "/v1/users/log/click", strings.NewReader(clickLog))
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
 
-	// req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
-	// rec := httptest.NewRecorder()
-	// c := e.NewContext(req, rec)
-	// h := &Handler{DB: mockDB}
+	mock := mock.NewMockLog(ctrl)
+	mock.EXPECT().StoreSerpEventLog(&serpEventLog).Return(nil)
 
-	// if assert.NoError(t, h.CreateTaskTimeLog(c)) {
-	// 	if diff := cmp.Diff(rec.Code, http.StatusCreated); diff != "" {
-	// 		t.Errorf("Status code does not match.\n%v", diff)
-	// 	}
-	// }
+	e := echo.New()
+	req := httptest.NewRequest(
+		http.MethodPost,
+		"/v1/logs/click",
+		strings.NewReader(serpEventLogBlob),
+	)
+
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	h := NewLogHandler(mock)
+
+	if assert.NoError(t, h.CreateSerpEventLog(c)) {
+		if diff := cmp.Diff(rec.Code, http.StatusCreated); diff != "" {
+			t.Fatal("Status code does not match.\n" + diff)
+		}
+	}
+}
+
+func TestStoreSearchSession(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mock := mock.NewMockLog(ctrl)
+	mock.EXPECT().StoreSearchSeeion(&searchSessionLog).Return(nil)
+
+	e := echo.New()
+	req := httptest.NewRequest(
+		http.MethodPost,
+		"/v1/logs/session",
+		strings.NewReader(searchSessionLogBlob),
+	)
+
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	h := NewLogHandler(mock)
+
+	if assert.NoError(t, h.StoreSearchSeeion(c)) {
+		if diff := cmp.Diff(rec.Code, http.StatusCreated); diff != "" {
+			t.Fatal("Status code does not match.\n" + diff)
+		}
+	}
 }
