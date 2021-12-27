77e6215132d5e1cd9a4e15ec23370cc3b0410df2
Yusuke Shimizu <44394399+ushmz@users.noreply.github.com>
2021-12-27 17:34:32 +0900

    :+1: Update tests to confirm errors

--- app/internal/handler/log_test.go
+++ app/internal/handler/log_test.go
@@ -1,111 +1,271 @@
 package handler
 
 import (
+	"bytes"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
-	"strings"
 	"testing"
 
 	"ratri/internal/domain/model"
+	"ratri/internal/handler"
 	mock "ratri/internal/mock/usecase"
 
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 	"github.com/labstack/echo/v4"
-	"github.com/stretchr/testify/assert"
 )
 
 var (
-	serpViewLog = model.SerpViewingLogParam{
-		UserId:      999,
-		TaskId:      5,
-		ConditionId: 3,
+	serpViewLogs = []struct {
+		name      string
+		in        model.SerpViewingLogParam
+		want      int
+		wantError bool
+		err       error
+	}{
+		{
+			"name",
+			model.SerpViewingLogParam{UserId: 999, TaskId: 5, ConditionId: 3},
+			201,
+			false,
+			nil,
+		},
 	}
-	serpViewLogBlob = `{
-		"user":      999,
-		"task":      5,
-		"condition": 3
-	}`
-	serpEventLog = model.SearchPageEventLogParam{
-		User:        42,
-		TaskId:      5,
-		ConditionId: 3,
-		Time:        42,
-		Page:        1,
-		Rank:        3,
-		IsVisible:   true,
-		Event:       "click",
+	pageViewLogs = []struct {
+		name      string
+		in        model.PageViewingLogParam
+		want      int
+		wantError bool
+		err       error
+	}{
+		{
+			"name",
+			model.PageViewingLogParam{UserId: 999, TaskId: 5, ConditionId: 3, PageId: 356},
+			201,
+			false,
+			nil,
+		},
 	}
-	serpEventLogBlob = `{
-		"user":      42,
-		"task":      5,
-		"condition": 3,
-		"time":      42,
-		"page":      1,
-		"rank":      3,
-		"visible":   true,
-		"event":     "click"
-	}`
-	searchSessionLog = model.SearchSession{
-		UserId:      42,
-		TaskId:      5,
-		ConditionId: 3,
+
+	serpEventLogs = []struct {
+		name      string
+		in        model.SearchPageEventLogParam
+		want      int
+		wantError bool
+		err       error
+	}{
+		{
+			"name",
+			model.SearchPageEventLogParam{
+				User:        42,
+				TaskId:      5,
+				ConditionId: 3,
+				Time:        42,
+				Page:        1,
+				Rank:        3,
+				IsVisible:   true,
+				Event:       "click",
+			},
+			201,
+			false,
+			nil,
+		},
 	}
-	searchSessionLogBlob = `{
-		"user":      42,
-		"task":      5,
-		"condition": 3
-	}`
+	searchSessionLogs = []struct {
+		name      string
+		in        model.SearchSession
+		want      int
+		wantError bool
+		err       error
+	}{
+		{
+			"name",
+			model.SearchSession{UserId: 42, TaskId: 5, ConditionId: 3},
+			201,
+			false,
+			nil,
+		},
+	}
 )
 
 func TestCreateTaskTimeLog(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
+	e := echo.New()
 	mock := mock.NewMockLog(ctrl)
-	mock.EXPECT().CumulateSerpViewingTime(&serpViewLog).Return(nil)
+	for _, tt := range serpViewLogs {
+		t.Run(tt.name, func(t *testing.T) {
+			mock.EXPECT().CumulateSerpViewingTime(&tt.in).Return(nil)
+
+			b, err := json.Marshal(tt.in)
+			if err != nil {
+				t.Fatal("Failed to marshal test case: %w\n", err)
+			}
+
+			req := httptest.NewRequest(
+				http.MethodPost,
+				"/v1/logs/time",
+				bytes.NewBuffer(b),
+			)
+
+			req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+			rec := httptest.NewRecorder()
+			c := e.NewContext(req, rec)
+			h := handler.NewLogHandler(mock)
+
+			err = h.CumulateSerpViewingTime(c)
+
+			// Throw t.Fatal if unexpected error has occurred.
+			if !tt.wantError && err != nil {
+				t.Fatalf("Want no error, but got %#v", err)
+			}
+
+			// Throw t.Fatal if different error has occurred.
+			if tt.wantError && !(err == tt.err) {
+				t.Fatalf("Want %#v, but got %#v", tt.err, err)
+			}
+
+			// Throw t.Fatal if expected value is different from result.
+			if diff := cmp.Diff(tt.want, rec.Code); !tt.wantError && diff != "" {
+				t.Fatalf("Want %d, but got %d\n%v", tt.want, rec.Code, diff)
+			}
+		})
+	}
+}
+
+func TestCumulateSerpViewingTime(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
 
 	e := echo.New()
-	req := httptest.NewRequest(
-		http.MethodPost,
-		"/v1/logs/time",
-		strings.NewReader(serpViewLogBlob),
-	)
+	mock := mock.NewMockLog(ctrl)
 
-	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
-	rec := httptest.NewRecorder()
-	c := e.NewContext(req, rec)
-	h := NewLogHandler(mock)
+	for _, tt := range serpViewLogs {
 
-	if assert.NoError(t, h.CumulateSerpViewingTime(c)) {
-		if diff := cmp.Diff(rec.Code, http.StatusCreated); diff != "" {
-			t.Fatal("Status code does not match.\n" + diff)
+		mock.EXPECT().CumulateSerpViewingTime(&tt.in).Return(nil)
+
+		b, err := json.Marshal(tt.in)
+		if err != nil {
+			t.Fatal("Failed to marshal test case: %w\n", err)
+		}
+
+		req := httptest.NewRequest(
+			http.MethodPost,
+			"/v1/logs/click",
+			bytes.NewBuffer(b),
+		)
+
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
+		h := handler.NewLogHandler(mock)
+
+		err = h.CumulateSerpViewingTime(c)
+
+		// Throw t.Fatal if unexpected error has occurred.
+		if !tt.wantError && err != nil {
+			t.Fatalf("Want no error, but got %#v", err)
+		}
+
+		// Throw t.Fatal if different error has occurred.
+		if tt.wantError && !(err == tt.err) {
+			t.Fatalf("Want %#v, but got %#v", tt.err, err)
+		}
+
+		// Throw t.Fatal if expected value is different from result.
+		if diff := cmp.Diff(tt.want, rec.Code); !tt.wantError && diff != "" {
+			t.Fatalf("Want %d, but got %d\n%v", tt.want, rec.Code, diff)
 		}
 	}
 }
 
-func TestCreateSerpClickLog(t *testing.T) {
+func TestCumulatePageViewingTime(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	e := echo.New()
+	mock := mock.NewMockLog(ctrl)
+
+	for _, tt := range pageViewLogs {
+		mock.EXPECT().CumulatePageViewingTime(&tt.in).Return(nil)
+		b, err := json.Marshal(tt.in)
+		if err != nil {
+			t.Fatal("Failed to marshal test case: %w\n", err)
+		}
+
+		req := httptest.NewRequest(
+			http.MethodPost,
+			"/v1/logs/click",
+			bytes.NewBuffer(b),
+		)
+
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
+		h := handler.NewLogHandler(mock)
+
+		err = h.CumulatePageViewingTime(c)
+
+		// Throw t.Fatal if unexpected error has occurred.
+		if !tt.wantError && err != nil {
+			t.Fatalf("Want no error, but got %#v", err)
+		}
+
+		// Throw t.Fatal if different error has occurred.
+		if tt.wantError && !(err == tt.err) {
+			t.Fatalf("Want %#v, but got %#v", tt.err, err)
+		}
+
+		// Throw t.Fatal if expected value is different from result.
+		if diff := cmp.Diff(tt.want, rec.Code); !tt.wantError && diff != "" {
+			t.Fatalf("Want %d, but got %d\n%v", tt.want, rec.Code, diff)
+		}
+	}
+}
+
+func TestCreateSerpEventLog(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
 	mock := mock.NewMockLog(ctrl)
-	mock.EXPECT().StoreSerpEventLog(&serpEventLog).Return(nil)
 
-	e := echo.New()
-	req := httptest.NewRequest(
-		http.MethodPost,
-		"/v1/logs/click",
-		strings.NewReader(serpEventLogBlob),
-	)
+	for _, tt := range serpEventLogs {
+		mock.EXPECT().StoreSerpEventLog(&tt.in).Return(nil)
 
-	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
-	rec := httptest.NewRecorder()
-	c := e.NewContext(req, rec)
-	h := NewLogHandler(mock)
+		b, err := json.Marshal(tt.in)
+		if err != nil {
+			t.Fatal("Failed to marshal test case: %w\n", err)
+		}
 
-	if assert.NoError(t, h.CreateSerpEventLog(c)) {
-		if diff := cmp.Diff(rec.Code, http.StatusCreated); diff != "" {
-			t.Fatal("Status code does not match.\n" + diff)
+		e := echo.New()
+		req := httptest.NewRequest(
+			http.MethodPost,
+			"/v1/logs/click",
+			bytes.NewBuffer(b),
+		)
+
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
+		h := handler.NewLogHandler(mock)
+
+		err = h.CreateSerpEventLog(c)
+
+		// Throw t.Fatal if unexpected error has occurred.
+		if !tt.wantError && err != nil {
+			t.Fatalf("Want no error, but got %#v", err)
+		}
+
+		// Throw t.Fatal if different error has occurred.
+		if tt.wantError && !(err == tt.err) {
+			t.Fatalf("Want %#v, but got %#v", tt.err, err)
+		}
+
+		// Throw t.Fatal if expected value is different from result.
+		if diff := cmp.Diff(tt.want, rec.Code); !tt.wantError && diff != "" {
+			t.Fatalf("Want %d, but got %d\n%v", tt.want, rec.Code, diff)
 		}
 	}
 }
@@ -115,23 +275,42 @@ func TestStoreSearchSession(t *testing.T) {
 	defer ctrl.Finish()
 
 	mock := mock.NewMockLog(ctrl)
-	mock.EXPECT().StoreSearchSeeion(&searchSessionLog).Return(nil)
 
-	e := echo.New()
-	req := httptest.NewRequest(
-		http.MethodPost,
-		"/v1/logs/session",
-		strings.NewReader(searchSessionLogBlob),
-	)
+	for _, tt := range searchSessionLogs {
+		mock.EXPECT().StoreSearchSeeion(&tt.in).Return(nil)
 
-	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
-	rec := httptest.NewRecorder()
-	c := e.NewContext(req, rec)
-	h := NewLogHandler(mock)
+		b, err := json.Marshal(tt.in)
+		if err != nil {
+			t.Fatal("Failed to marshal test case: %w\n", err)
+		}
 
-	if assert.NoError(t, h.StoreSearchSeeion(c)) {
-		if diff := cmp.Diff(rec.Code, http.StatusCreated); diff != "" {
-			t.Fatal("Status code does not match.\n" + diff)
+		e := echo.New()
+		req := httptest.NewRequest(
+			http.MethodPost,
+			"/v1/logs/session",
+			bytes.NewBuffer(b),
+		)
+
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
+		h := handler.NewLogHandler(mock)
+
+		err = h.StoreSearchSeeion(c)
+
+		// Throw t.Fatal if unexpected error has occurred.
+		if !tt.wantError && err != nil {
+			t.Fatalf("Want no error, but got %#v", err)
+		}
+
+		// Throw t.Fatal if different error has occurred.
+		if tt.wantError && !(err == tt.err) {
+			t.Fatalf("Want %#v, but got %#v", tt.err, err)
+		}
+
+		// Throw t.Fatal if expected value is different from result.
+		if diff := cmp.Diff(tt.want, rec.Code); !tt.wantError && diff != "" {
+			t.Fatalf("Want %d, but got %d\n%v", tt.want, rec.Code, diff)
 		}
 	}
 }
